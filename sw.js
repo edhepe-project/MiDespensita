9de9ccfb67c52d52f83752e0e0a5a651e04ecae4
edhepe-project <edhepe-project@users.noreply.github.com>
2026-07-27 12:07:35 -0600

    Crop icon to make it larger on mobile screens and bump cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'midespensita-v7';
+const CACHE_NAME = 'midespensita-v8';
 
 // Rutas relativas para GitHub Pages
 const BASE_URL = self.location.pathname.replace(/\/[^/]*$/, '/');
